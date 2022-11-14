a07266fc392af9ba87e9bde2ac42d91f84ccb3ee
VictorCid <victorcidpereira@gmail.com>
2022-11-14 07:06:58 -0300

    enrollments

---
+++ src/protocols.ts
@@ -0,0 +1,36 @@
+export type ApplicationError = {
+  name: string;
+  message: string;
+};
+
+export type ViaCEPAddress = {
+  logradouro: string,
+  complemento: string,
+  bairro: string,
+  cidade: string,
+  uf: string,
+};
+
+export type RequestError = {
+  status: number,
+  data: object | null,
+  statusText: string,
+  name: string,
+  message: string,
+};
+
+export type ViaCEPAddressAPI = {
+  cep: number,
+  logradouro: string,
+  complemento: string,
+  bairro: string,
+  localidade: string,
+  uf: string,
+  ibge: string,
+  gia: string,
+  ddd: string,
+  siafi: string
+}
+
+// export type ViaCEPAddress = Omit<ViaCEPAddressDB, "ibge" | "gia" | "ddd" | "siafi">
+
